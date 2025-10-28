a2409c235bc344c4e58f25fd8897618972d900ae
BradyDouthit <brady.douthit99@gmail.com>
2025-10-28 19:21:38 -0400

    output word frequency from data

--- perf/word_frequency.js
+++ perf/word_frequency.js
@@ -1,10 +1,36 @@
 import fs from "node:fs";
 
 const filepath = "data.txt";
+const topN = 30;
 
 try {
   const data = fs.readFileSync(filepath, "utf8");
-  console.log(data);
+  const words = data.split(" ");
+
+  const wordMap = new Map();
+
+  // get counts for each word
+  words.forEach((word) => {
+    const mappedWord = wordMap.get(word);
+
+    if (!mappedWord) {
+      wordMap.set(word, 1);
+      return;
+    }
+
+    wordMap.set(word, mappedWord + 1);
+  });
+
+  const sorted = Array.from(wordMap).sort((a, b) => {
+    const aCount = a[1];
+    const bCount = b[1];
+    return bCount - aCount;
+  });
+  const topWords = sorted.slice(0, topN);
+
+  topWords.forEach(([word, count]) => {
+    console.log(`${word}: ${count}`);
+  });
 } catch (err) {
   console.error(err);
 }
